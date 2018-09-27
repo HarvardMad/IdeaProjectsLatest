19a390b727e3fe97b994832d7113d5c3210d5dee
Lalin <laleendra@gmail.com>
2018-09-27 22:32:40 +0100

    added test

---
+++ Revolut/src/test/java/RevolutTests.java
@@ -0,0 +1,28 @@
+import com.revolut.accounts.dao.AccountsDAO;
+import com.revolut.accounts.datamodel.CurrentAccount;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RevolutTests {
+  private static final AccountsDAO accountsDAO = new AccountsDAO();
+
+  @Before
+  public void init(){
+    accountsDAO.createNewAccount("Adam",400.00,"12345");
+    accountsDAO.createNewAccount("Zac",250.23,"54321");
+  }
+
+  @Test
+  public void happy_path() throws InterruptedException, IOException{
+    CurrentAccount from = accountsDAO.getAccount("12345");
+    CurrentAccount to = accountsDAO.getAccount("54321");
+    CurrentAccount.startMoneyTransferProcess(from,to,100.00);
+    Assert.assertEquals(300.00,from.getBalanceAmount(),000.00);
+    Assert.assertEquals(350.23,to.getBalanceAmount(),000.00);
+
+
+  }
+
+}
